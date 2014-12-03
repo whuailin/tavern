ec63cb18c43686b1defef887d10a42f613a119d7
whuailin <whuailin@126.com>
2014-12-03 20:44:24 +0800

    project init

---
+++ Assets/script/BusinessMgr.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BusinessMgr : MonoBehaviour {
+    public Slider processBar;
+
+    public int dayTime = 100;
+    public enum BusinessStatus { wait, in_business };
+
+    private BusinessStatus curStatus;
+    private float curTime;
+
+    private Text text;
+
+	// Use this for initialization
+	void Start () {
+        curStatus = BusinessStatus.wait;
+        curTime = 0.0f;
+        text = GameObject.Find("BusinessTxt").GetComponent<Text>();
+        processBar.value = dayTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (curStatus == BusinessStatus.in_business)
+        {
+            Application.LoadLevel(1);
+            if (curTime > dayTime)
+            {
+                curTime = 0;
+                curStatus = BusinessStatus.wait;
+                text.text = "等待开张.....";
+            }
+            else
+            {
+                curTime += Time.deltaTime;
+                text.text = "开张中： " + (int)curTime;
+                processBar.value = processBar.value - 1 * Time.deltaTime;
+            }
+
+        }
+        else
+        {
+            text.text = "等待开张.....";
+        }
+	}
+
+    public void starBusiness()
+    {
+        curStatus = BusinessStatus.in_business;
+    }
+}
---
+++ Assets/script/MapEditor/MapEditorEngine.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapEditorEngine : MonoBehaviour {
+
+	public string mapName;
+
+    private enum states {
+        none,
+        tChoose,
+        tMove,
+        tRoate,
+        tBuild,
+        tErase,
+        camMove,
+        camZoom
+    };
+
+    private GameObject curTile;
+    private string curFamilyName;
+    private int lastSelectIndex;
+    private int globalGridSizeX;
+    private int globalGridSizeZ;
+    private Vector3 gridSize;
+    private int curObjID;
+    private int curLayer;
+    private states optState;
+
+    private GameObject Map;
+    private List<GameObject> tcGoes = new List<GameObject>();
+    public GameObject grid;
+
+    public GameObject cameraGO;
+    public class TileInfo {
+        public string catName;
+        public string catCollision;
+        public List<GameObject> catObjs = new List<GameObject>();
+        public List<Texture2D> catObjsPrevs = new List<Texture2D>();
+        public List<string> catObjsNames = new List<string>();
+        public List<string> catGuidNames = new List<string>();
+
+        public TileInfo(string _catName, List<string> _catObjsNames, string _catCollision, List<GameObject> _catObjs, List<Texture2D> _catObjsPrevs, List<string> _catGuidNames)
+        {
+            catName = _catName;
+            catCollision = _catCollision;
+            catObjs = _catObjs;
+            catObjsPrevs = _catObjsPrevs;
+            catObjsNames = _catObjsNames;
+            catGuidNames = _catGuidNames;
+        }
+    }
+
+    void Start()
+    {
+        initMapEditorEngine();
+
+        loadTools();
+    }
+
+    public void initMapEditorEngine() {
+        Map = new GameObject("MAP");
+        globalGridSizeZ = 1000;
+        globalGridSizeX = 1000;
+        gridSize = new Vector3(1000.0f, 0.1f, 1000.0f);
+
+        cameraGO.transform.Rotate(new Vector3(90.0f, 0.0f, 0.0f));
+        cameraGO.transform.position = new Vector3(500, 14, 490);
+
+        optState = states.none;
+    }
+
+    private void loadTools() {
+        GameObject _grid = (GameObject)Resources.Load("uteLayer");
+        grid = (GameObject)Instantiate(_grid, new Vector3((gridSize.x / 2) + 0.5f, 0.0f, (gridSize.z / 2) + 0.5f), _grid.transform.rotation);
+        grid.name = "Grid";
+        setGrid(globalGridSizeX, globalGridSizeZ);
+    }
+
+    private void setGrid(int x, int z) {
+        grid.transform.localScale = new Vector3((float)x, 0.01f, (float)z);
+        grid.renderer.material.mainTextureScale = new Vector2((float)x, (float)z);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        switch (optState)
+        {
+            case states.none:
+                break;
+            case states.tChoose:
+                break;
+            case states.tMove:
+                break;
+            case states.tRoate:
+                break;
+            case states.tBuild:
+                break;
+            case states.tErase:
+                break;
+            case states.camMove:
+                break;
+            case states.camZoom:
+                break;
+            default:
+                break;
+        }
+	}
+
+    void foo() {
+        Ray builldRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit buildHit;
+
+        if (Physics.Raycast(builldRay, out buildHit, 1000))
+        {
+            if (buildHit.collider)
+            {
+                GameObject hitObj = buildHit.collider.gameObject;
+
+            } 
+        }
+    }
+}
